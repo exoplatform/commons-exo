7733deb3fd15e8dc87ac74461650c5749215f849
Romain Dénarié <romain.denarie@exoplatform.com>
2023-03-08 09:13:25 +0100

    Remove not necessary code

--- commons-component-product/src/main/java/org/exoplatform/commons/info/MissingProductInformationException.java
+++
@@ -1,9 +0,0 @@
-package org.exoplatform.commons.info;
-
-public class MissingProductInformationException extends Exception {
-  private static final long serialVersionUID = -7407551830430394842L;
-
-  public MissingProductInformationException(String propertyName) {
-    super("Missing product information property: " + propertyName);
-  }
-}
